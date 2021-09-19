e4a585228818198f447cf8eb57689f57e6609cca
Oscar Villarragaclear <dvillarraga@gsoftcolombia.com>
2021-09-18 21:00:43 -0500

    adding cloudformation_pipeline

---
+++ vars/cloudformation_pipeline.groovy
@@ -0,0 +1,19 @@
+def call(Map pipelineParams) {
+    pipeline {
+        agent any
+        options {
+            withAWS(credentials: 'cloudformation', region: 'us-east-1')
+        }
+        environment {
+            stackName = "${pipelineParams.stackName}"
+        }
+        stages {
+            stage('Template Validation') {
+                steps {
+                    def response = cfnValidate(file:"${pipelineParams.templateFile}")
+                    echo "Template Description: ${response.description}"
+                }
+            }
+        }
+    }
+}
